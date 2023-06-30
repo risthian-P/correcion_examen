78f630ce2b95a2f2a8bca40766023352edb75301
Cristhian Panora <cristhian.panora@gmail.com>
2023-06-30 12:01:08 -0500

    clases y menu añadidos

--- src/principal.java
+++ src/principal.java
@@ -6,6 +6,9 @@ public class principal {
         int dato = 1;
         int i, nhabit;
         String pais, contin, ciudad,canton, parroquia;
+
+        //objetos e instancias
+
         parroquia a[] = new parroquia[dato];
         cantones b[] = new cantones[dato];
         ciudades c[] = new ciudades[dato];
@@ -13,7 +16,10 @@ public class principal {
 
         System.out.println("ingresar el numero de datos a ingresar: ");
         dato = entrada.nextInt();
+        entrada.nextLine();
+
         for (i=0;i<=dato;i++){
+            System.out.println("ingreso numero "+ (i+1));
             System.out.println("ingresar el pais");
             pais = entrada.nextLine();
             System.out.println("ingresar el continente");
@@ -22,15 +28,20 @@ public class principal {
             ciudad = entrada.nextLine();
             System.out.println("ingresar el n habitantes");
             nhabit = entrada.nextInt();
+            entrada.nextLine();
             System.out.println("ingresar el canton ");
             canton = entrada.nextLine();
             System.out.println("ingresar el parroquia");
             parroquia = entrada.nextLine();
-            entrada.nextLine();
+
             a[i] = new parroquia(pais,contin,ciudad,nhabit,canton,parroquia);
+
         }
-        for(i=0;i<=dato;i++){
-            System.out.println(a[1].getNombpais() + ", " + a[i].getNomcontinente() + ", " + a[i].getNombciudad() + ", " + a[i].getNhabitantes() + ", " + a[i].getNombcanton() + ", " + a[i].getNombparroquia());
+
+        for(i=0; i<=dato ;i++){
+
+            System.out.println(a[i].getNombpais() + ", " + a[i].getNomcontinente() + ", " + a[i].getNombciudad() + ", " + a[i].getNhabitantes() + ", " + a[i].getNombcanton() + ", " + a[i].getNombparroquia());
+
         }
     }
 }
